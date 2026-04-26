21a5587624620f2d539f4acc04155464f5e0f7d9
NotApoopy <omizeeshan1110@gmail.com>
2026-04-26 21:00:41 +0500

    Add files via upload

---
+++ ThirdJScript.js
@@ -0,0 +1,6 @@
+let userage
+document.createElement("TagNamw")
+document.getElementById("SubmitButton").onclick = function() {
+    userage = document.getElementById("AgeEnter");
+    console.log(`you are ${userage.value} years old`);
+}
